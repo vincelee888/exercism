01f3474ee032d802376d7e502137181ed9dafdf9
Vince Lee <vslee888@gmail.com>
2018-10-27 20:48:28 +0100

    fix function return types

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -1,12 +1,12 @@
 ﻿module Bob
 open System
 
-let getTotalChars (input) = 
+let getTotalChars (input) : int = 
     input
     |> Seq.filter(fun c -> Char.IsLetter(c))
     |> Seq.length
 
-let isShouting (input:string) bool =
+let isShouting (input:string) : bool =
   let totalChars = getTotalChars input
   let totalUpperCaseChars = 
     input
@@ -15,19 +15,19 @@ let isShouting (input:string) bool =
 
   totalChars > 0 && totalUpperCaseChars = totalChars
 
-let isQuestion (input:string) bool =
+let isQuestion (input:string) : bool =
   input.EndsWith "?"
 
-let isForceFulQuestion (input:string) bool =
-  let a = isShouting(input) () // this is weird, why do i have to use ()?
-  let b = isQuestion(input) ()
+let isForceFulQuestion (input:string) : bool =
+  let a = isShouting(input)
+  let b = isQuestion(input)
 
   a && b
 
-let isSilence (input:string) bool =
+let isSilence (input:string) : bool =
   String.IsNullOrWhiteSpace input
 
-type InputResponsePair = { predicate: string -> unit -> bool; response: string }
+type InputResponsePair = { predicate: string -> bool; response: string }
 let response (input: string): string = 
   let inputResponsePairs = [
     { predicate = isForceFulQuestion; response = "Calm down, I know what I'm doing!" };
@@ -38,7 +38,7 @@ let response (input: string): string =
   let trimmed = input.TrimEnd()
   let response = 
     inputResponsePairs
-    |> Seq.tryFind(fun pair -> pair.predicate(trimmed)())
+    |> Seq.tryFind(fun pair -> pair.predicate(trimmed))
 
   match response with
   | Some pair -> pair.response
