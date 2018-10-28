58e4d1c921d932e6e4bb3488f53e573fc711a0b3
Vince Lee <vslee888@gmail.com>
2018-10-28 10:12:54 +0000

    fsharp/sum of multiples

---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,13 @@
+﻿module SumOfMultiples
+
+let rec isMultipleOf(value: int, numbers: int list): bool =
+  match numbers with
+  | head :: tail when value % head <> 0 -> isMultipleOf (value, tail)
+  | head :: _ when value % head = 0 -> true
+  | _ -> false
+
+let sum (numbers: int list) (upperBound: int): int =
+  [0..upperBound]
+    |> List.filter (fun x -> x < upperBound)
+    |> List.filter (fun x -> isMultipleOf(x, numbers))
+    |> List.sum
