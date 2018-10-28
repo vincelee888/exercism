26363056f4a53d45047ef84684654fb0ac0d8d4e
Vince Lee <vslee888@gmail.com>
2018-10-28 15:23:16 +0000

    fsharp/twofer

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,9 @@
+﻿module TwoFer
+
+let twoFer (input: string option): string =
+  let name = 
+    match input with
+    | Some value -> value
+    | None -> "you"
+
+  sprintf "One for %s, one for me." name
