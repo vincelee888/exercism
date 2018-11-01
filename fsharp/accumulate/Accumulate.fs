083f732874a7a36927a2b169b118c64caa1e937b
Vince Lee <vslee888@gmail.com>
2018-11-01 19:03:25 +0000

    without using library map function!

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -1,5 +1,8 @@
 ﻿module Accumulate
 
 let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
-  input
-  |> List.map func
+  let rec myMap f acc = function 
+    | [] -> acc
+    | head :: tail -> myMap f (acc @ [ f head]) tail
+  
+  myMap func [] input
