d6e153183b29c5347f2e310af51bca65d4e2a484
Vince Lee <vslee888@gmail.com>
2018-11-03 12:55:10 +0000

    make tail recursive

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -3,6 +3,7 @@
 let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
   let rec myMap f acc = function 
     | [] -> acc
-    | head :: tail -> myMap f (acc @ [ f head]) tail
+    | head :: tail -> myMap f (f head :: acc) tail
   
   myMap func [] input
+  |> List.rev
